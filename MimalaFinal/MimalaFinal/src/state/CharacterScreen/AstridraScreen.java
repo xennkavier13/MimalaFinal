635e621cbf319df695b010cf9009715be3573a55
Gerard <gerard081304@gmail.com>
2025-04-02 23:30:25 +0800

    "Added Back and Continue Buttons but not completed yet"

--- MimalaFinal/MimalaFinal/src/state/CharacterScreen/AstridraScreen.java
+++ MimalaFinal/MimalaFinal/src/state/CharacterScreen/AstridraScreen.java
@@ -1,19 +1,81 @@
 package state.CharacterScreen;
 
+import state.CharacterSelection;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class AstridraScreen extends JPanel{
+    private final ImageIcon astridraGif;
+    private final JFrame frame;
 
     public AstridraScreen(JFrame frame) {
-        setLayout(new BorderLayout());
+        this.frame = frame;
+        astridraGif = new ImageIcon("MimalaFinal\\MimalaFinal\\src\\assets\\CharacterSelectionScreen\\CharacterScreen\\AstridraSelection.gif");
+        setPreferredSize(new Dimension(1920, 1080));
+        setupButtons();
+    }
 
-        // Load the looping GIF
-        ImageIcon astridraGif = new ImageIcon("MimalaFinal\\MimalaFinal\\src\\assets\\CharacterSelectionScreen\\CharacterScreen\\AstridraSelection.gif");
-        JLabel gifLabel = new JLabel(astridraGif);
-        gifLabel.setHorizontalAlignment(JLabel.CENTER);
+    private void setupButtons() {
+        int backButtonX = 100;
+        int backButtonY = 200;
+        int continueButtonX = 70;
+        int continueButtonY = 500;
 
-        add(gifLabel, BorderLayout.CENTER);
+        JLabel backButton = createButton(
+                "C:\\Users\\User\\IdeaProjects\\MimalaFinal\\MimalaFinal\\MimalaFinal\\src\\assets\\CharacterSelectionScreen\\CharacterScreenButtons\\Back\\Back_off.png",
+                "C:\\Users\\User\\IdeaProjects\\MimalaFinal\\MimalaFinal\\MimalaFinal\\src\\assets\\CharacterSelectionScreen\\CharacterScreenButtons\\Back\\Back_hover.png",
+                backButtonX, backButtonY, () -> {
+                    frame.setContentPane(new CharacterSelection(frame, "PVP")); // Pass mode
+                    frame.revalidate();
+                    frame.repaint();
+                }
+        );
+
+        JLabel continueButton = createButton(
+                "C:\\Users\\User\\IdeaProjects\\MimalaFinal\\MimalaFinal\\MimalaFinal\\src\\assets\\CharacterSelectionScreen\\CharacterScreenButtons\\Continue\\Continue_off.png",
+                "C:\\Users\\User\\IdeaProjects\\MimalaFinal\\MimalaFinal\\MimalaFinal\\src\\assets\\CharacterSelectionScreen\\CharacterScreenButtons\\Continue\\Continue_hover.png ",
+                continueButtonX, continueButtonY, () -> {
+                    frame.setContentPane(new CharacterSelection(frame, "PVC")); // Pass mode
+                    frame.revalidate();
+                    frame.repaint();
+                }
+        );
+
+        add(backButton);
+        add(continueButton);
+    }
+
+    private JLabel createButton(String offPath, String hoverPath, int y, int backButtonY, Runnable action) {
+        ImageIcon offIcon = new ImageIcon(offPath);
+        ImageIcon hoverIcon = new ImageIcon(hoverPath);
+        JLabel button = new JLabel(offIcon);
+        //button.setBounds(970, y, offIcon.getIconWidth(), offIcon.getIconHeight());
+
+        button.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent e) {
+                button.setIcon(hoverIcon);
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent e) {
+                button.setIcon(offIcon);
+            }
+
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent e) {
+                action.run();
+            }
+        });
+        return button;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(astridraGif.getImage(), 0, 0, getWidth(), getHeight(), this);
     }
 }
+
 
--- MimalaFinal/MimalaFinal/src/state/CharacterScreen/AzuroxScreen.java
+++ MimalaFinal/MimalaFinal/src/state/CharacterScreen/AzuroxScreen.java
@@ -1,18 +1,78 @@
 package state.CharacterScreen;
 
+import state.CharacterSelection;
+
 import javax.swing.*;
 import java.awt.*;
-
 public class AzuroxScreen extends JPanel{
+    private final ImageIcon azuroxGif;
+    private final JFrame frame;
 
     public AzuroxScreen(JFrame frame) {
-        setLayout(new BorderLayout());
+        this.frame = frame;
+        azuroxGif = new ImageIcon("MimalaFinal\\MimalaFinal\\src\\assets\\CharacterSelectionScreen\\CharacterScreen\\AzuroxSelection.gif");
+        setPreferredSize(new Dimension(1920, 1080));
+        setupButtons();
+    }
 
-        // Load the looping GIF
-        ImageIcon azuroxGif = new ImageIcon("MimalaFinal\\MimalaFinal\\src\\assets\\CharacterSelectionScreen\\CharacterScreen\\AzuroxSelection.gif");
-        JLabel gifLabel = new JLabel(azuroxGif);
-        gifLabel.setHorizontalAlignment(JLabel.CENTER);
+    private void setupButtons() {
+        int backButtonX = 100;
+        int backButtonY = 200;
+        int continueButtonX = 70;
+        int continueButtonY = 500;
 
-        add(gifLabel, BorderLayout.CENTER);
+        JLabel backButton = createButton(
+                "C:\\Users\\User\\IdeaProjects\\MimalaFinal\\MimalaFinal\\MimalaFinal\\src\\assets\\CharacterSelectionScreen\\CharacterScreenButtons\\Back\\Back_off.png",
+                "C:\\Users\\User\\IdeaProjects\\MimalaFinal\\MimalaFinal\\MimalaFinal\\src\\assets\\CharacterSelectionScreen\\CharacterScreenButtons\\Back\\Back_hover.png",
+                backButtonX, backButtonY, () -> {
+                    frame.setContentPane(new CharacterSelection(frame, "PVP")); // Pass mode
+                    frame.revalidate();
+                    frame.repaint();
+                }
+        );
+
+        JLabel continueButton = createButton(
+                "C:\\Users\\User\\IdeaProjects\\MimalaFinal\\MimalaFinal\\MimalaFinal\\src\\assets\\CharacterSelectionScreen\\CharacterScreenButtons\\Continue\\Continue_off.png",
+                "C:\\Users\\User\\IdeaProjects\\MimalaFinal\\MimalaFinal\\MimalaFinal\\src\\assets\\CharacterSelectionScreen\\CharacterScreenButtons\\Continue\\Continue_hover.png ",
+                continueButtonX, continueButtonY, () -> {
+                    frame.setContentPane(new CharacterSelection(frame, "PVC")); // Pass mode
+                    frame.revalidate();
+                    frame.repaint();
+                }
+        );
+
+        add(backButton);
+        add(continueButton);
+    }
+
+    private JLabel createButton(String offPath, String hoverPath, int y, int backButtonY, Runnable action) {
+        ImageIcon offIcon = new ImageIcon(offPath);
+        ImageIcon hoverIcon = new ImageIcon(hoverPath);
+        JLabel button = new JLabel(offIcon);
+        //button.setBounds(970, y, offIcon.getIconWidth(), offIcon.getIconHeight());
+
+        button.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent e) {
+                button.setIcon(hoverIcon);
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent e) {
+                button.setIcon(offIcon);
+            }
+
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent e) {
+                action.run();
+            }
+        });
+        return button;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(azuroxGif.getImage(), 0, 0, getWidth(), getHeight(), this);
     }
 }
